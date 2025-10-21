edf8fbfefd0576e7578e039bdc8e634c8b11fd56
Ahmad Kafin <user@Users-MacBook-Pro.local>
2025-10-21 09:34:36 +0700

    add cors to server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
+
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
